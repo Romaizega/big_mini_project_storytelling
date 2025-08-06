21c710f3fd7cf5d1e3d5465270f67da0f8b64192
Roman Izegov <izegovrg@yandex.ru>
2025-08-06 15:03:18 +0300

    fixed auth_router added /

--- src/backend/routers/auth_router.js
+++ src/backend/routers/auth_router.js
@@ -3,6 +3,6 @@ const {register} = require('../controllers/auth_controller')
 
 const router = express.Router()
 
-router.post('register', register)
+router.post('/register', register)
 
 module.exports = router
